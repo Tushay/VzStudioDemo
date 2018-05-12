f9cf1880b944d974b8530dcdae9512834890d598
Adam Clark <yahsut123@gmail.com>
2018-05-12 00:01:51 -0500

    Changed text

--- VzStudioDemo/Program.cs
+++ VzStudioDemo/Program.cs
@@ -2,11 +2,12 @@
 
 namespace VzStudioDemo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vz Studio Demo");
+            Console.ReadLine();
         }
     }
 }
